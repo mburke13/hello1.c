a0c07aa497a7320fe46c040a8453d21a7ee4eed0
mburke13 <59965569+mburke13@users.noreply.github.com>
2020-01-16 10:21:09 -0600

    Fixed Errors

--- hello.c
+++ hello.c
@@ -12,12 +12,12 @@ This program will print my name
 #include <stdio.h>
 
 //this will create the main variable allowing me to print my name
-int main {
-//this will be the print function and lower the curser
-printf("Maddy Burke/n");
-printf("Math Major/n");
+int main(void) {
+	//this will be the print function and lower the curser
+	printf("Maddy Burke\n");
+	printf("Math Major\n");
     
     printf("My name is Andrew Persson\n");
     printf("My major is mechanical engineering\n");
-return 0:
+	return 0;
 }
