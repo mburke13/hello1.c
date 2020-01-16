65912594d3e94089d098f607cf0d4e57c5df969a
mburke13 <59965569+mburke13@users.noreply.github.com>
2020-01-16 10:26:10 -0600

    Added readme

--- hello.c
+++ hello.c
@@ -14,10 +14,10 @@ This program will print my name
 //this will create the main variable allowing me to print my name
 int main(void) {
 	//this will be the print function and lower the curser
-	printf("Maddy Burke\n");
-	printf("Math Major\n");
+printf("Maddy Burke\n");
+printf("Math Major\n");
     
     printf("My name is Andrew Persson\n");
     printf("My major is mechanical engineering\n");
-	return 0;
+return 0;
 }
