71979c6523e625ae23be8587e9856da35f40e19d
mburke13 <59965569+mburke13@users.noreply.github.com>
2020-01-16 08:44:58 -0600

    Initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/* 
+Author: Maddy Burke
+
+1/1/19
+
+Collaberators: 
+
+This program will print my name
+*/
+
+//this will allow the scanf and printf functions
+#include <stdio.h>
+
+//this will create the main variable allowing me to print my name
+int main {
+//this will be the print function and lower the curser
+printf("Maddy Burke/n");
+return 0:
+}
